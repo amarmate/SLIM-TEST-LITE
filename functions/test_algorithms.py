dabb8bdf1de56a9e7faadb6243fe4202fa7fe992
Mateus Amaral <mbaptistaamaral@gmail.com>
2024-12-04 15:36:17 +0000

    corrected functionality of results

--- functions/test_algorithms.py
+++ functions/test_algorithms.py
@@ -17,8 +17,6 @@ def test_slim(X, y, args_dict=None,
             dataset_name='dataset_1', 
             pop_size=100, 
             n_iter=100, 
-            ms_lower=0, 
-            ms_upper=1, 
             n_elites=1,
             initializer='rhh',
             iterations=30,
@@ -26,7 +24,6 @@ def test_slim(X, y, args_dict=None,
             algorithm="SLIM+SIG1",
             verbose=0,
             p_train=0.7,
-            tournament_size=2,
             show_progress=True,
             log = False
 ):    
@@ -46,10 +43,6 @@ def test_slim(X, y, args_dict=None,
         The population size.
     n_iter: int     
         The number of iterations to perform.
-    ms_lower: float
-        The lower bound for the mutation strength.
-    ms_upper: float
-        The upper bound for the mutation strength.
     n_elites: int
         The number of elites.
     initializer: str
@@ -66,8 +59,6 @@ def test_slim(X, y, args_dict=None,
         The verbosity level.
     p_train: float
         The percentage of the training set.
-    tournament_size: int
-        The tournament size.
     show_progress: bool
         Whether to show the progress bar or not.
     log: bool
@@ -97,9 +88,8 @@ def test_slim(X, y, args_dict=None,
         A list containing the size of the trees.
     representations: list
         A list containing the tree representations.
-    """
-    
-    rmse_, mae_, mape_, rmse_compare, mae_compare, mape_compare, time_stats, train_fit, test_fit, size, representations = [], [], [], [], [], [], [], [], [], [], []
+    """    
+    rmse_, mae_, mape_, rmse_comp, mae_comp, mape_comp, time_stats, train_fit, test_fit, size, representations = [], [], [], [], [], [], [], [], [], [], []
 
     for it in tqdm(range(iterations), disable=not show_progress):
         X_train, X_test, y_train, y_test = train_test_split(X, y, p_test=1-p_train, seed=it)
@@ -119,17 +109,17 @@ def test_slim(X, y, args_dict=None,
             
         start = time.time()
         final_tree = slim(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                            dataset_name=dataset_name, slim_version=algorithm, pop_size=pop_size, n_iter=n_iter, seed=it, ms_lower=ms_lower, ms_upper=ms_upper,
-                            reconstruct=True, n_jobs=1, tournament_size=tournament_size, initializer=initializer,
-                            log_path=path, verbose=verbose,n_elites=n_elites, **args_dict, log_level=(3 if log else 0))
+                            dataset_name=dataset_name, slim_version=algorithm, pop_size=pop_size, n_iter=n_iter, seed=it,
+                            reconstruct=True, n_jobs=1, initializer=initializer,
+                            verbose=verbose, n_elites=n_elites, **args_dict, log_level=(3 if log else 0), log_path=(path if log else None))
         end = time.time()
-        
+                
         # Get the node count of the tree
         nodes_count = final_tree.nodes_count
         time_taken = end - start
         train_fitness_elite = final_tree.fitness.item()
         test_fitness_elite = final_tree.test_fitness.item()
-
+        
         # Calculate predictions and metrics
         y_pred = final_tree.predict(X_test)
         rmse_score = rmse(y_test, y_pred).item()
@@ -138,28 +128,35 @@ def test_slim(X, y, args_dict=None,
         rmse_compare = rmse_score
         mae_compare = mae_score
         mape_compare = mape_score
-
+        
         if scale:
-            y_pred_descaled = scaler_y.inverse_transform(y_pred.reshape(-1, 1)).reshape(-1)
-            y_test_descaled = scaler_y.inverse_transform(y_test.reshape(-1, 1)).reshape(-1)
+            # Convert tensors to numpy arrays for inverse scaling
+            y_pred_np = y_pred.cpu().detach().numpy()
+            y_test_np = y_test.cpu().detach().numpy()
+
+            # Apply inverse scaling
+            y_pred_descaled = torch.tensor(scaler_y.inverse_transform(y_pred_np.reshape(-1, 1)).reshape(-1))
+            y_test_descaled = torch.tensor(scaler_y.inverse_transform(y_test_np.reshape(-1, 1)).reshape(-1))
+
+            # Calculate metrics
             rmse_compare = rmse(y_test_descaled, y_pred_descaled).item()
             mae_compare = mae(y_test_descaled, y_pred_descaled)
             mape_compare = mape(y_test_descaled, y_pred_descaled)
-        
+                
         # Append metrics to respective lists
         rmse_.append(rmse_score)
         mape_.append(mape_score)
         mae_.append(mae_score)
-        rmse_compare.append(rmse_compare)
-        mape_compare.append(mape_compare)
-        mae_compare.append(mae_compare)
+        rmse_comp.append(rmse_compare)
+        mape_comp.append(mape_compare)
+        mae_comp.append(mae_compare)
         time_stats.append(time_taken)
         train_fit.append(train_fitness_elite)
         test_fit.append(test_fitness_elite)
         size.append(nodes_count)
         representations.append(final_tree.get_tree_representation())
-
-    return rmse_, mape_, mae_, rmse_compare, mape_compare, mae_compare, time_stats, train_fit, test_fit, size, representations
+        
+    return rmse_, mape_, mae_, rmse_comp, mape_comp, mae_comp, time_stats, train_fit, test_fit, size, representations
 
 
 # ----------------------------------- GSGP ----------------------------------- #
--- results.py
+++ results.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import time
+import random
 
 # Limit threads for NumPy and other multi-threaded libraries
 os.environ["OMP_NUM_THREADS"] = "1"  # Limit OpenMP threads to 1
@@ -28,6 +29,31 @@ def process_dataset(args):
     dataset_loader, scale, struct_mutation, xo, mut_xo = args
     X, y = dataset_loader()
     dataset_name = dataset_loader.__name__.split('load_')[1]
+    
+    # Get the suffixes for the file name
+    scale_suffix = 'scaled' if scale else 'unscaled'
+    xo_suffix = 'xo' if xo else 'no_xo'
+    gp_xo_suffix = 'mut_xo' if mut_xo else 'no_mut_xo'
+    struct_mutation_suffix = 'struct_mutation' if struct_mutation else 'no_struct_mutation'
+    
+    # Check if the directory exists
+    if not os.path.exists('results/slim'):
+        try:
+            os.makedirs('results/slim')
+        except Exception as e:
+            print(f"Failed to create 'results/slim' directory: {e}")
+            exit(1)
+            
+    # Check if there are already results for the dataset
+    try:
+        with open(f'results/slim/{dataset_name}_{scale_suffix}_{xo_suffix}_{gp_xo_suffix}_{struct_mutation_suffix}.pkl', 'rb') as f:
+            pickle.load(f)
+        print(f"File already exists: {dataset_name}_{scale_suffix}_{xo_suffix}_{gp_xo_suffix}_{struct_mutation_suffix}.pkl")
+        return
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(f"Error while checking file existence: {e}")
 
     # Log when the process starts 
     start_time = time.time()
@@ -38,53 +64,45 @@ def process_dataset(args):
     with open('time_logs.txt', 'a') as f:
         f.write(f"{dataset_name}_{scale}_{xo}_{mut_xo}_{struct_mutation},{start_time}\n")
 
-    # Get the suffixes for the file name
-    scale_suffix = 'scaled' if scale else 'unscaled'
-    xo_suffix = 'xo' if xo else 'no_xo'
-    gp_xo_suffix = 'mut_xo' if mut_xo else 'no_mut_xo'
-    struct_mutation_suffix = 'struct_mutation' if struct_mutation else 'no_struct_mutation'
-
     # Get the parameters from the random search
     try:
         with open(f'params/{dataset_name}_{scale_suffix}_{xo_suffix}_{gp_xo_suffix}_{struct_mutation_suffix}.pkl', 'rb') as f:
-            results = pickle.load(f)
+            params = pickle.load(f)
     except Exception as e:
         print(f"Failed to load file for {dataset_name}: {e}")
         return
-    
+        
     # Define the dictionary to store the results
     metrics = ['rmse', 'mape', 'mae', 'rmse_compare', 'mape_compare', 'mae_compare', 'time', 'train_fit', 'test_fit', 'size', 'representations']
     results = {metric: {} for metric in metrics}
 
-    for algorithm in results:
-        # Test SLIM
-        rm, mp, ma, rm_c, mp_c, ma_c, time, train, test, size, reps = test_slim(
-            X=X, y=y, args_dict=results[algorithm], dataset_name=dataset_loader.__name__,
-            ms_lower=0, ms_upper=1, n_elites=1,
-            iterations=n_samples, struct_mutation=struct_mutation, scale=scale, algorithm=algorithm,
-            verbose=0, p_train=p_train, show_progress=False,
-        )
-
-        # Store the results in a compact manner
-        results['rmse'][algorithm] = rm
-        results['mape'][algorithm] = mp
-        results['mae'][algorithm] = ma
-        results['rmse_compare'][algorithm] = rm_c
-        results['mape_compare'][algorithm] = mp_c
-        results['mae_compare'][algorithm] = ma_c
-        results['time'][algorithm] = time
-        results['train_fit'][algorithm] = train
-        results['test_fit'][algorithm] = test
-        results['size'][algorithm] = size
-        results['representations'][algorithm] = reps
-
-    # Save the results in results/slim
-    if not os.path.exists('results/slim'):
-        try:
-            os.makedirs('results/slim')
-        except Exception as e:
-            print(f"Failed to create 'results/slim' directory: {e}")
-            exit(1)
+    try:
+        for algorithm in params:
+            # Clean the dictionary
+            params_clean = {k: (v.item() if isinstance(v, (np.float64, np.int64)) else v) for k, v in params[algorithm].items()}
+            
+            # Test SLIM
+            rm, mp, ma, rm_c, mp_c, ma_c, time_stats, train, test, size, reps = test_slim(
+                X=X, y=y, args_dict=params_clean, dataset_name=dataset_loader.__name__,
+                n_iter=n_iter, pop_size=pop_size, n_elites=1, iterations=n_samples, scale=scale, 
+                algorithm=algorithm, verbose=0, p_train=p_train, show_progress=True,
+            )
+            
+            # Store the results in a compact manner
+            results['rmse'][algorithm] = rm
+            results['mape'][algorithm] = mp
+            results['mae'][algorithm] = ma
+            results['rmse_compare'][algorithm] = rm_c
+            results['mape_compare'][algorithm] = mp_c
+            results['mae_compare'][algorithm] = ma_c
+            results['time'][algorithm] = time_stats
+            results['train_fit'][algorithm] = train
+            results['test_fit'][algorithm] = test
+            results['size'][algorithm] = size
+            results['representations'][algorithm] = reps
+    except Exception as e:
+        print(f"Error in testing SLIM: {e}")
+        return
 
     try:
         with open(f'results/slim/{dataset_name}_{scale_suffix}_{xo_suffix}_{gp_xo_suffix}_{struct_mutation_suffix}.pkl', 'wb') as f:
@@ -110,6 +128,8 @@ if __name__ == '__main__':
     tasks = [(loader, True, False, False, False) for loader in datasets] + [(loader, False, False, False, False) for loader in datasets]
     tasks += [(loader, True, True, False, False) for loader in datasets] + [(loader, True, False, True, False) for loader in datasets]
     tasks += [(loader, True, True, True, False) for loader in datasets] + [(loader, True, True, True, True) for loader in datasets]
+    
+    random.shuffle(tasks)
 
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(process_dataset, task) for task in tasks]
