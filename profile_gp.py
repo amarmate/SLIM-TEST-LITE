555d401a95e72c5a56c5e60e0a2409d83b1b084e
Mateus Amaral <mbaptistaamaral@gmail.com>
2025-05-19 20:35:36 +0100

    Update profile_gp.py

--- profile_gp.py
+++ profile_gp.py
@@ -23,7 +23,7 @@ def dataset3(n=500, seed=0, noise=0):
         y += np.random.normal(0,(noise/100)*np.std(y),size=n)
     return train_test_split(x,y,p_test=0.2,seed=0)
 
-Xtr, ytr, Xte, yte = dataset3(n=2000, seed=0, noise=0)
+Xtr, Xte, ytr, yte = dataset3(n=2000, seed=0, noise=0)
 
 def measure(seed, label=None):
     proc = psutil.Process()
